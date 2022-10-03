35a364c624650f5f30c111471a9c7f281af76d4e
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-10-03 14:30:00 +0300

    LevelSave fixed

--- RollerClone/Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ RollerClone/Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -33,19 +33,34 @@ public class LevelGenerator : MonoBehaviour
 
     public void SaveSeed()
     {
+        levelSeeds.Clear();
+        levelHeight.Clear();
+        levelWidth.Clear();
+        for (int i = 0; i < PlayerPrefs.GetInt("Count",0); i++)
+        {
+            Debug.Log("firstAdd");
+            levelSeeds.Add(PlayerPrefs.GetInt("Seed" + i, 0));
+            levelHeight.Add(PlayerPrefs.GetInt("Height" + i, 0));
+            levelWidth.Add(PlayerPrefs.GetInt("Width" + i, 0));
+        }
+        
+
         levelSeeds.Add(_seed);
         levelHeight.Add(GridManager.Instance.gridHeight);
         levelWidth.Add(GridManager.Instance.gridWidth);
-        PlayerPrefs.SetInt("Count", levelSeeds.Count);
+        
         for (int i = 0; i < levelSeeds.Count; i++)
         {
+            Debug.Log("secondAdd");
+            PlayerPrefs.SetInt("Seed" + i, levelSeeds[i]);
+                PlayerPrefs.SetInt("Height" + i, levelHeight[i]);
+                PlayerPrefs.SetInt("Width" + i, levelWidth[i]);
+
             
-            PlayerPrefs.SetInt("Seed"+i, levelSeeds[i]);
-            PlayerPrefs.SetInt("Height" + i, levelHeight[i]);
-            PlayerPrefs.SetInt("Width" + i, levelWidth[i]);
+
 
         }
-       
+        PlayerPrefs.SetInt("Count", levelSeeds.Count);
     }  
     
 
